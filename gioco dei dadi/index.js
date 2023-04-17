2edf998a65f61b063ffbf0f85e4e176cdae39f18
antonionicolaci <antonionicolaci48@gmail.com>
2023-04-17 16:11:08 +0200

    add style

--- gioco dei dadi/index.js
+++ gioco dei dadi/index.js
@@ -3,8 +3,13 @@ function randomFunction() { // funzione per il tiro di dado
     const randomUtente = Math.floor(Math.random() * 6) + 1;
 
     if (randomPC > randomUtente) {
-        document.getElementById("random_game").innerHTML = "Ha vinto il banco";
+        document.getElementById("random_game").innerHTML = `<span id="banco_vince">Ha vinto il banco</span>`;
     } else {
-        document.getElementById("random_game").innerHTML = "Ha vinto il giocatore";
+        document.getElementById("random_game").innerHTML = `<span id="utente_vince">Ha vinto il giocatore</span>`;
     }
+
+    document.getElementById("span_utente").innerHTML = "Il tuo numero è:";
+    document.getElementById("span_utente_numero").innerHTML = randomUtente;
+    document.getElementById("span_pc").innerHTML = "Il numero del banco è:";
+    document.getElementById("span_pc_numero").innerHTML = randomPC;
 }
