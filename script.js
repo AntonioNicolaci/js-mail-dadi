34da64cb1d3e97f8f1121dcf69714f0b960c6c34
antonionicolaci <antonionicolaci48@gmail.com>
2023-04-17 15:35:20 +0200

    fix for arrey.length dont show last email in array

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ function emailFunction() { // funzione che controlla se l'email è presente nel 
     const email = document.getElementById("email").value; // prende il valore del campo nella pagina web che corrisponde all'email.
     let controllo_email = true; // se questa variabile diventa falsa attiverà un if che specifica che questa email non è presente nel database.
 
-    for (let i = 0; [database_email.length - 1] > i; i++) { // questo ciclo for contine un BREAK, che si attiva se l'email inserita è nel database
+    for (let i = 0; database_email.length > i; i++) { // questo ciclo for contine un BREAK, che si attiva se l'email inserita è nel database
         if (database_email[i] == email) {
-            document.getElementById("email_result").innerHTML = "Email nella lista, accesso a breve... forse";
+            document.getElementById("email_result").innerHTML = `<span id="email_giusta">Email nella lista, accesso a breve... forse</span>`;
             controllo_email = true; // Se l'email non è presente il programma non entrerà mai qui dentro e la variabile non diventerà mai falsa
             break;
         } else {
@@ -24,7 +24,7 @@ function emailFunction() { // funzione che controlla se l'email è presente nel 
         }
 
         if (controllo_email == false) { // entrare dentro questo if comporta che non c'erano email corrispondenti
-            document.getElementById("email_result").innerHTML = "Email non presente nella lista"
+            document.getElementById("email_result").innerHTML = `<span id="email_sbagliata">Email non presente nella lista</span>`;
         }
     }
 }
